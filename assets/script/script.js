f748c5def09c919f350b4468a45db6b5fe0443b8
Tyler <Tyl3r.s@outlook.com>
2022-05-25 16:52:16 -0400

    added prompts

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 // password character variables
-var characterLength = 8;
+var characterLength = [];
 var choiceArray = [];
 
 var specialCharacterArray = ['!','@','#','$','%','^','&','*','(',')','_','<','>','|'];
@@ -9,30 +9,56 @@ var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o
 var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numberArray = ['1','2','3','4','5','6','7','8','9','0'];
 
+// prompts function 
+
+function promptValues() {
+  choiceArray = [];
+  characterLength = parseInt(prompt("How many characters should be included in your password? Please choose any amount betwen 8 and 128."));
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Total number of characters must be between 8 and 128. Please input a valid amount."); 
+    return false;
+  }
+  if (confirm("Do you wish to include lowercase characters?")) {
+    choiceArray = choiceArray.concat(lowerCaseArray);
+  }
+  if (confirm("Do you wish to include uppercase characters?")) {
+    choiceArray = choiceArray.concat(upperCaseArray);
+  }
+  if (confirm("Do you wish to include special characters?")) {
+    choiceArray = choiceArray.concat(specialCharacterArray);
+  }
+  if (confirm("Do you wish to include numbers?")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+  return true;
+  
+}
+
 // password based on prompts
 function generatePassword() {
     var password = "";
     for(var i = 0; i < characterLength; i++) {
-      var randomIndex = math.floor(Math.random() * choiceArray.length);
+      var randomIndex = Math.floor(Math.random() * choiceArray.length);
       password = password + choiceArray[randomIndex];
     }
     return password;
 }
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password2 = generatePassword();
+  var correctPrompts = promptValues();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password2;
-
+  if (correctPrompts) {
+    var finalPassword = generatePassword();
+    passwordText.value = finalPassword;
+  } 
+  else {
+    passwordText.value = "";
+  }
 }
 
 // Add event listener to generate button
