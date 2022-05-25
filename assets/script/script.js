9e087bce3b54d08b9cb2f5e2b291c9e92b2687e6
Tyler <Tyl3r.s@outlook.com>
2022-05-25 15:36:50 -0400

    add variables and first function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharacterArray = ['!','@','#','$','%','^','&','*','(',')','_','<','>','|'];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
